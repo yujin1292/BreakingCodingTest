60b489714bc79b50447533da6a519be8264e59cf
nawonhee <65769072+nawonhee@users.noreply.github.com>
2021-10-31 02:03:08 +0900

    Add files via upload

---
+++ HomeWork/WonHee/[week 1]/11651.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool compare(pair<int, int> a, pair<int, int> b) {
+	if (a.second < b.second)
+		return true;
+	if (a.second == b.second)
+		if (a.first < b.first)
+			return true;
+	return false;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n;
+	vector<pair<int, int>> v;
+
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		int x, y;
+		cin >> x >> y;
+		v.push_back({ x, y });
+	}
+	sort(v.begin(), v.end(), compare);
+
+	for (int i = 0; i < n; i++) {
+		cout << v[i].first << " " << v[i].second << "\n";
+	}
+
+	system("pause");
+	return 0;
+}
