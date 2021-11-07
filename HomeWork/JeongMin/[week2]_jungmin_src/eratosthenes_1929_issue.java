83766ee2f6fa2929e1d5f06dcf0a47247c89e559
jungmin <kimjm0825@gmail.com>
2021-11-08 00:57:30 +0900

    add issue code - eratosthenes_1929_issue

---
+++ HomeWork/JeongMin/[week2]_jungmin_src/eratosthenes_1929_issue.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class eratosthenes_1929_issue {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader((System.in)));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int start = Integer.parseInt(st.nextToken());
+        int end = Integer.parseInt(st.nextToken());
+
+        sieve(start, end);
+    }
+
+    public static void sieve(int start, int end) {
+        StringBuilder sb = new StringBuilder();
+
+        boolean[] sieve = new boolean[end+1];
+
+        sieve[0] = sieve[1] = false;
+        for (int i = start; i <= end; i++) sieve[i] = true;
+
+        for (int i = 2; i <= Math.sqrt(end); i++) {
+            for (int j = i; i*j <= end; j++) {
+                sieve[i*j] = false;
+            }
+        }
+
+        for (int i = start; i <= end; i++) {
+            if (sieve[i]) sb.append(i).append('\n');
+        }
+
+        System.out.println(sb);
+    }
+}
